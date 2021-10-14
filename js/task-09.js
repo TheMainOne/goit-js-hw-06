53e6e18513d3fd7939a6690370514a553d84777a
Maksym Lvov <leva880@ukr.net>
2021-10-14 12:55:13 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const button = document.querySelector('.change-color');
+const valueOfColor = document.querySelector('.color');
+const body = document.body;
+// console.log(body);
+
+button.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  valueOfColor.textContent = body.style.backgroundColor;
+};
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
