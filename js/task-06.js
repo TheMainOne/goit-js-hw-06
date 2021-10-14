7b33452f753b89aedfebbca33bbf1e813b3a81d9
Maksym Lvov <leva880@ukr.net>
2021-10-14 11:29:14 +0300

    task 6 done

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('input[data-length]');
+
+const onValidInputValue = (event) => {
+    event.currentTarget.classList.add('valid');
+    if (event.currentTarget.value.length >= 6) {
+        event.currentTarget.classList.add('valid');
+        event.currentTarget.classList.replace('invalid', 'valid');
+    } else {
+        event.currentTarget.classList.add('invalid');
+        event.currentTarget.classList.replace('valid', 'invalid');
+    }
+};
+
+input.addEventListener('blur', onValidInputValue);
