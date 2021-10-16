c0a6c5e465cdf24e52cb743b036f1965616a2a03
Maksym Lvov <leva880@ukr.net>
2021-10-16 11:02:55 +0300

    task 7 maked some changes

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
 const changeTextFontStyle = (event) => {
-  text.style.fontSize = event.currentTarget.value + "px";
+  text.style.fontSize = `${event.currentTarget.value}px`;
 };
 
-input.addEventListener("change", changeTextFontStyle);
+input.addEventListener("input", changeTextFontStyle);
